dbe1c1f4315c455fe265b0782e40ae42a92c4776
User <User@LAB3-3>
2024-01-15 12:23:21 +0200

    qqq

--- OnlineDanceStore/Services/OnlineDanceStoreServices.cs
+++ OnlineDanceStore/Services/OnlineDanceStoreServices.cs
@@ -26,9 +26,9 @@ namespace OnlineDanceStore.Services
           Quantity= 5,
           Price= 200,
           Image="imagestringshoes" }};
-      
-        
-       
+
+
+
         #endregion
 
         readonly HttpClient _httpClient;
@@ -44,43 +44,43 @@ namespace OnlineDanceStore.Services
                 ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve
             };
 
-           
+
         }
-            #region
-            public async Task<UserDto> LoginAsync(string email, string password)
+        #region
+        public async Task<UserDto> LoginAsync(string email, string password)
+        {
+            try
             {
-                try
+                //האובייקט לשליחה
+                User user = new User() { Email = email, UserPswd = password };
+                //מבצעת סיריליזציה
+                var jsonContent = JsonSerializer.Serialize(user, _serializerOptions);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync($"{URL}Login", content);
+
+                switch (response.StatusCode)
                 {
-                    //האובייקט לשליחה
-                    User user = new User() { Email = email, UserPswd = password };
-                    //מבצעת סיריליזציה
-                    var jsonContent = JsonSerializer.Serialize(user, _serializerOptions);
-                    var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-                    var response = await _httpClient.PostAsync($"{URL}Login", content);
+                    case (HttpStatusCode.OK):
+                        {
+                            jsonContent = await response.Content.ReadAsStringAsync();
+                            User u = JsonSerializer.Deserialize<User>(jsonContent, _serializerOptions);
+                            await Task.Delay(2000);
+                            return new UserDto() { Success = true, Message = string.Empty, User = u };
 
-                    switch (response.StatusCode)
-                    {
-                        case (HttpStatusCode.OK):
-                            {
-                                jsonContent = await response.Content.ReadAsStringAsync();
-                                User u = JsonSerializer.Deserialize<User>(jsonContent, _serializerOptions);
-                                await Task.Delay(2000);
-                                return new UserDto() { Success = true, Message = string.Empty, User = u };
+                        }
+                    case (HttpStatusCode.Unauthorized):
+                        {
+                            return new UserDto() { Success = false, User = null, Message = ErrorMessages.INVALID_LOGIN };
 
-                            }
-                        case (HttpStatusCode.Unauthorized):
-                            {
-                                return new UserDto() { Success = false, User = null, Message = ErrorMessages.INVALID_LOGIN };
-
-                            }
-
-                    }
+                        }
 
                 }
-                catch (Exception ex) { Console.WriteLine(ex.Message); }
-                return new UserDto() { Success = false, User = null, Message = ErrorMessages.INVALID_LOGIN };
 
             }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return new UserDto() { Success = false, User = null, Message = ErrorMessages.INVALID_LOGIN };
+
+        }
 
         public async Task<UserDto> RegisterUser(User user)
         {
@@ -100,8 +100,8 @@ namespace OnlineDanceStore.Services
                         {
                             User u;
                             jsonContent = await response.Content.ReadAsStringAsync();
-                           u = JsonSerializer.Deserialize<User>(jsonContent, _serializerOptions);
-                            return new UserDto {Success=true, Message=null, User=u };
+                            u = JsonSerializer.Deserialize<User>(jsonContent, _serializerOptions);
+                            return new UserDto { Success = true, Message = null, User = u };
 
                         }
                     case (HttpStatusCode.Conflict):
@@ -116,8 +116,14 @@ namespace OnlineDanceStore.Services
             return null;
 
         }
+
+        public async Task<List<Item>> GetItems(int CategoryId)
+        {
+            return items.Where(x=> x.Categories.Id == CategoryId).ToList();
+        }
+           
         #endregion
 
-    }
+    } 
     }
 
