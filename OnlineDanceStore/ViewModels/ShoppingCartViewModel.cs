29e7d97e59e9ab7e2653e11a798dafcaa0ae081e
User <User@LAB3-3>
2024-01-31 14:53:31 +0200

    cart

--- OnlineDanceStore/ViewModels/ShoppingCartViewModel.cs
+++ OnlineDanceStore/ViewModels/ShoppingCartViewModel.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using OnlineDanceStore.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using OnlineDanceStore.Models;
+using OnlineDanceStore.View;
+using System.Text.Json;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
 
 namespace OnlineDanceStore.ViewModels
 {
     public class ShoppingCartViewModel:ViewModel
     {
+        #region Fields
+        private Item newitem;
+
+        #region Properties
+        public Item NewItem { get => newitem; set { newitem = value; } }
+     
+        public ObservableCollection<Item> Items { get; set; }
+        #endregion
+        #endregion
+        #region Service component
+        private readonly OnlineDanceStoreServices _service;
+        #endregion
+        #region Commands
+        public ICommand AddToCartCommand { get; protected set; }
+        #endregion
+        public List<Item> ItemsInTheCart { get; set; } = new List<Item>();
+        public ShoppingCartViewModel(OnlineDanceStoreServices service) 
+        {
+            _service = service;
+            NewItem = new Item();
+            AddToCartCommand= new Command(async () =>
+            {
+                try
+                {
+                    ItemsInTheCart.Add(NewItem);
+                }
+                catch (Exception ex) { }
+            });
+        }
     }
 }
