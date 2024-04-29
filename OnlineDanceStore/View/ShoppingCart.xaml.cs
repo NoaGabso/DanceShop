c52f1393aa6b58622bdbb83dbc70c0319008f055
noaga <gabso.noa@gmail.com>
2024-04-29 20:58:32 +0300

    edd

--- OnlineDanceStore/View/ShoppingCart.xaml.cs
+++ OnlineDanceStore/View/ShoppingCart.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Maui.Views;
 using OnlineDanceStore.ViewModels;
 
 namespace OnlineDanceStore.View;
@@ -20,5 +21,20 @@ public partial class ShoppingCart : ContentPage
 			
 
 		}
+    }
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        if (sender is ImageButton button && button.CommandParameter is string imagePath)
+        {
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                this.ShowPopup(new PopUpPage(imagePath));
+            }
+            else
+            {
+                Console.WriteLine("Image source is not available");
+                // Handle this case accordingly
+            }
+        }
     }
 }
