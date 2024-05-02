5fa9d9623c00667f1123a3a01c2eaebe9ebf3448
User <User@LAB3-3>
2024-05-02 14:04:56 +0300

    property

--- OnlineDanceStore/ViewModels/AdminPageViewModel.cs
+++ OnlineDanceStore/ViewModels/AdminPageViewModel.cs
@@ -9,6 +9,7 @@ using OnlineDanceStore.View;
 using OnlineDanceStore.Services;
 using System.Text.Json;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace OnlineDanceStore.ViewModels
 {
@@ -151,10 +152,153 @@ namespace OnlineDanceStore.ViewModels
             }
         }
 
+        
+          public int ItemGenderId
+        { get => genderid;
+            set
+            {
+                {
+                    if (gendername == "men")
+                        genderid = 1;
+                    else
+                        genderid = 2;
+                }
+                OnPropertyChange();
+            }
+        }
 
-        #endregion
+        public Gender ItemGender
+        { get => gender; set
+            {
+                { gender.GenderId = genderid; gender.GenderName = gendername; }
+                OnPropertyChange();
+            }
+            }
 
-        private readonly OnlineDanceStoreServices _service;
+        public int ItemCategortId
+        {
+            get => categoriesid;
+            set
+            {
+                switch (categoriesname)
+                {
+                    case "Leotards":
+                        categoriesid = 1; break;
+                    case "DanceShoes":
+                        categoriesid = 2; break;
+                    case "Accessories":
+                        categoriesid = 3; break;
+                    case "DanceClothes":
+                        categoriesid = 4; break;
+                }
+                OnPropertyChange();
+            }
+        }
+           public Categories ItemCategory
+        {
+            get => categories;
+            set
+            {
+                {
+                    categories.CategoryId = categoriesid;
+                    categories.CategoriesName = categoriesname;
+                }
+                OnPropertyChange();
+            }
+            }
+
+        public int ItemSizeId
+        {
+            get => sizeid;
+            set
+            {
+                switch (sizename)
+                {
+                    case "Small":
+                        sizeid = 1; break;
+                    case "Medium":
+                        sizeid = 2; break;
+                    case "Large":
+                        sizeid = 3; break;
+                    case "35":
+                        sizeid = 4; break;
+                    case "36":
+                        sizeid = 5; break;
+                    case "37":
+                        sizeid = 6; break;
+                    case "38":
+                        sizeid = 7; break;
+                    case "39":
+                        sizeid = 8; break;
+                    case "40":
+                        sizeid = 9; break;
+                    case "41":
+                        sizeid = 10; break;
+                    case "42":
+                        sizeid = 11; break;
+                    case "43":
+                        sizeid = 12; break;
+                    case "44":
+                        sizeid = 13; break;
+                }
+                OnPropertyChange ();
+
+            }
+        }
+
+        public SizeItem ItemSize
+        {
+            get=> size;
+            set
+            {
+                size.SizeItemId = sizeid;
+                size.SizeName = sizename;
+                OnPropertyChange ();
+            } 
+        }
+
+        public int ItemColorId
+        {
+            get => colorid;
+            set
+            {
+                switch (colorname)
+                {
+                    case "Black":
+                        sizeid = 1; break;
+                    case "White":
+                        sizeid = 2; break;
+                    case "Maroon":
+                        sizeid = 3; break;
+                    case "DeepBlue":
+                        sizeid = 4; break;
+                    case "Olive":
+                        sizeid = 5; break;
+                    case "BabyPink":
+                        sizeid = 6; break;
+                    case "LightPurple":
+                        sizeid = 7; break;
+                    case "LightBlue":
+                        sizeid = 8; break;
+                }
+                OnPropertyChange ();
+            }
+        }
+
+        public ColorItem ItemColor
+        { get => color;
+        set
+            {
+                color.ColorName = colorname;
+                color.ColorItemId = colorid;
+                OnPropertyChange();
+            }
+             }
+
+
+#endregion
+
+private readonly OnlineDanceStoreServices _service;
         public ICommand NewItemCommand { get; protected set; }
 
         //public bool IsButtonEnabled
@@ -167,101 +311,12 @@ namespace OnlineDanceStore.ViewModels
 
             _service = service;
             itemname = string.Empty;
+            description = string.Empty;
+            categoriesname = string.Empty;
             gendername = string.Empty;
-
-
-
-            {
-                if (gendername == "men")
-                    genderid = 1;
-                else
-                    genderid = 2;
-            }
-
-            switch (categoriesname)
-            {
-                case "Leotards":
-                    categoriesid = 1; break;
-                case "DanceShoes":
-                    categoriesid = 2; break;
-                case "Accessories":
-                    categoriesid = 3; break;
-                case "DanceClothes":
-                    categoriesid = 4; break;
-            }
-            categories = new Categories();
-            {
-                categories.CategoryId = categoriesid;
-                categories.CategoriesName = categoriesname;
-            }
-
-            gender = new Gender();
-            {
-                gender.GenderId = genderid;
-                gender.GenderName = gendername;
-            }
-
-            switch (sizename)
-            {
-                case "Small":
-                    sizeid = 1; break;
-                case "Medium":
-                    sizeid = 2; break;
-                case "Large":
-                    sizeid = 3; break;
-                case "35":
-                    sizeid = 4; break;
-                case "36":
-                    sizeid = 5; break;
-                case "37":
-                    sizeid = 6; break;
-                case "38":
-                    sizeid = 7; break;
-                case "39":
-                    sizeid = 8; break;
-                case "40":
-                    sizeid = 9; break;
-                case "41":
-                    sizeid = 10; break;
-                case "42":
-                    sizeid = 11; break;
-                case "43":
-                    sizeid = 12; break;
-                case "44":
-                    sizeid = 13; break;
-            }
-
-            size = new SizeItem();
-            {
-                size.SizeItemId = sizeid;
-                size.SizeName = sizename;
-            }
-
-            switch (colorname)
-            {
-                case "Black":
-                    sizeid = 1; break;
-                case "White":
-                    sizeid = 2; break;
-                case "Maroon":
-                    sizeid = 3; break;
-                case "DeepBlue":
-                    sizeid = 4; break;
-                case "Olive":
-                    sizeid = 5; break;
-                case "BabyPink":
-                    sizeid = 6; break;
-                case "LightPurple":
-                    sizeid = 7; break;
-                case "LightBlue":
-                    sizeid = 8; break;
-            }
-
-            color = new ColorItem();
-            {
-                color.ColorName = colorname;
-                color.ColorItemId = colorid;
-            }
+            sizename = string.Empty;
+            colorname = string.Empty;
+            image = string.Empty;
 
 
             NewItemCommand = new Command(async () =>
